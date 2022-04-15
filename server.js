b2ce95ac21879dc28fa777e23027fc2afd879f88
Oleksiy Andryushayev <linuks454@gmail.com>
2022-04-15 09:16:27 +0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/shelf-optimizer'));
+app.use(express.static(__dirname + '/dist/shelf-optimiser'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+ '/dist/shelf-optimizer/index.html'));});
+res.sendFile(path.join(__dirname+ '/dist/shelf-optimiser/index.html'));});
 app.listen(process.env.PORT || 8080);
